90a0cbfeb22fb9b85fd06993c4bc6c477e328172
ever user <everuser@MacBook-Air-ever.local>
2020-12-04 09:04:15 +0200

    update layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,17 @@
-import './helpers/postDate';
-import scrollSmooth from './helpers/smooth-scroll.js';
-import Select from './modules/ Select';
+import "./helpers/postDate";
 
-function main() {
-  scrollSmooth();
-  Select.init();
-}
+function main() {}
 
 if (document.documentElement.clientWidth < 480) {
-  window.addEventListener('scroll',
+  window.addEventListener(
+    "scroll",
     function () {
       return setTimeout(main, 1000);
-    }, {
-      once: true
-    });
+    },
+    {
+      once: true,
+    }
+  );
 } else {
   main();
-};
+}
